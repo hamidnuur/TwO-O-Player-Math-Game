5cec18f4ee7fb363ddda3f97d38e0c1bd7d7e545
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-09 16:58:55 -0500

    initialize game and players

--- game.rb
+++ game.rb
@@ -1,6 +1,16 @@
 class Game
-  attr_reader :name
+  attr_reader :name, :player_1, :player_2
+
   def initialize(name)
     @name = name
+    @player_1 = Player.new('player_1')
+    @player_2 = Player.new('player_2')
+  end
+
+  def start
+    puts "New Game Started. Welcome #{player_1.name} and #{player_2.name}"
+    pp @player_1
+    pp @player_2
+    # turn
   end
 end

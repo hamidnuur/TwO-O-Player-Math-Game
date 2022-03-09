f3e89061d78a795f1f963e8be0c96583f4c55364
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-09 17:19:55 -0500

    refactored code + removed attr_reader

--- game.rb
+++ game.rb
@@ -1,20 +1,41 @@
 class Game
-  attr_reader :name, :player_1, :player_2
-
   def initialize(name)
     @name = name
-    @player_1 = Player.new('player_1')
-    @player_2 = Player.new('player_2')
+    @player_1 = Player.new('Player 1')
+    @player_2 = Player.new('Player 2')
   end
 
   def start
-    puts "New Game Started. Welcome #{player_1.name} and #{player_2.name}"
-    pp @player_1
-    pp @player_2
-    # turn
+    puts "New Game Started. Welcome #{@player_1.name} and #{@player_2.name}"
+    turn
   end
 
-  def show_score
-    puts "P1: #{player_1.lives}/3 vs P2: #{player_2.lives}/3"
+  def show_stats
+    puts "P1: #{@player_1.lives}/3 vs P2: #{@player_2.lives}/3"
+  end
 
+  def winner(player)
+    puts "#{player.name} wins with the score of #{player.lives}/3"
+    puts '---------GAME-OVER--------'
+    puts 'Good bye!'
+    exit(0)
+  end
+
+  def check_score
+    if @player_1.is_dead
+      winner(@player_2)
+    elsif @player_2.is_dead
+      winner(@player_1)
+    end
+  end
+
+  def turn
+    @player_1.new_question
+    check_score
+    @player_2.new_question
+    check_score
+    show_stats
+    puts '---------NEW-TURN---------'
+    turn
+  end
 end
